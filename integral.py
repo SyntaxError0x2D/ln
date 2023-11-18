2cc058b4c11810eb0434618aa61e8d26e294d7a2
sandem45 <116912414+SyntaxError0x2D@users.noreply.github.com>
2023-11-19 01:07:01 +0200

    Update integral.py
    Fixed embarrassing mistake for the case when n < 1.

--- integral.py
+++ integral.py
@@ -1,5 +1,4 @@
 #defining the natrual logarithm trough 1/x with integral area properties
-#needs case for when n < 1 with some stuff flipped
 
 iterLim = 2000
 
@@ -8,15 +7,7 @@ def ln(n : float):
         if n == 0: raise(Exception("ln(0) is undefined"))
         else: raise(Exception("ln(x), x<0 is undefined"))
 
-    elif n > 1:
-        h = (n-1)/iterLim ; s = 0
-        for i in range(iterLim):
-            s+=h*(1/(1+h*i))
-        return(s)
-    
-    elif n < 1:
-        h = (1-n)/iterLim ; s = 0
-        for i in range(iterLim):
-            s-=h*(1/(n+h*i))
-        return(s)
-    
+    h = (n-1)/iterLim ; s = 0
+    for i in range(iterLim):
+        s+=h*(1/(1+h*i))
+    return(s)
